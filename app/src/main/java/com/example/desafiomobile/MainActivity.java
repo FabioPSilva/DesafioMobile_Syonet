76843d8af01768bc7adfe33c29f488042ed03f76
Fabio Silva <f4b1m3@gmail.com>
2021-09-16 16:58:46 -0300

    Testando amend

--- app/src/main/java/com/example/desafiomobile/MainActivity.java
+++ app/src/main/java/com/example/desafiomobile/MainActivity.java
@@ -32,47 +32,59 @@ public class MainActivity extends AppCompatActivity {
         Button butOpenMap = findViewById(R.id.buttonOpenMap);
         ImageButton butGetAtualLocation = findViewById(R.id.butGetAtualLocation);
 
+        //Instancia a classe GetGPS passando o contexto
         getGPS = new GetGPS(MainActivity.this);
 
+        //Ação do botão de Usar a localização atual
         butGetAtualLocation.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                double latitude = getGPS.getLatitude();
-                double longitude = getGPS.getLongitude();
+                if(getGPS.checkPermission()) {
+                    //Obtem as coordenadas atuais
+                    double latitude = getGPS.getLatitude();
+                    double longitude = getGPS.getLongitude();
 
-                txtLatitude.setText(Double.toString(latitude));
-                txtLongitude.setText(Double.toString(longitude));
+                    //Joga as coordenadas nos campos de texto
+                    txtLatitude.setText(Double.toString(latitude));
+                    txtLongitude.setText(Double.toString(longitude));
+                }else{
+                    Toast.makeText(MainActivity.this, "Você deve permitir o uso do GPS pelo aplicativo!", Toast.LENGTH_SHORT).show();
+                }
 
                 //Toast.makeText(getApplicationContext(), latitude+","+longitude , Toast.LENGTH_SHORT).show();
             }
         });
 
+        //Ação do botão de abrir o mapa com as coordenadas
         butOpenMap.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                //Verifica a validação dos campos
                 if(validationCoordinate(txtLatitude.getText().toString(),txtLongitude.getText().toString())) {
+                    //Abre o mapa
                     Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("geo:" + txtLatitude.getText().toString() + "," + txtLongitude.getText().toString() + "?z=15"));
                     startActivity(intent);
                 }else{
+                    //Aviso de coordenadas invalidas
                     Toast.makeText(MainActivity.this, "Entre com um valor válido para a Latitude e Longitude", Toast.LENGTH_LONG).show();
                 }
             }
         });
 
-        //Solicita a permissão ao iniciar o app
+        //Solicita a permissão para uso do GPS ao iniciar o app
         askPermission();
     }
 
     public boolean validationCoordinate(String lat, String lon){
-        //lat +90 -90 lon +180 -180
         //Validar latitude
         Pattern patLat = Pattern.compile("^-?[0-9]{1,2}(?:\\.[0-9]{1,10})?$");
         //Validar longitude
         Pattern patLon = Pattern.compile("^-?[0-9]{1,3}(?:\\.[0-9]{1,10})?$");
-        //System.out.println(patLat.matcher(lat).find()&&patLon.matcher(lon).find());
+        //Retorna validação
         return (patLat.matcher(lat).find()&&patLon.matcher(lon).find());
     }
 
+    //Metodo que solicita permissão
     private void askPermission(){
         String[] permissions = new String[]{Manifest.permission.ACCESS_FINE_LOCATION,Manifest.permission.ACCESS_COARSE_LOCATION};
         ActivityCompat.requestPermissions(this,permissions,PERMISSIONS_CODE);
@@ -80,6 +92,15 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public void onRequestPermissionsResult(int requestCode, String[] permissions,int[] grantResults) {
-        System.out.println(requestCode);
+        boolean permissionOK = true;
+        for(int i=0;i<grantResults.length;i++){
+            if(grantResults[i] < 0){
+                permissionOK = false;
+            }
+        }
+
+        if(permissionOK){
+            getGPS.getLocation();
+        }
     }
 }
